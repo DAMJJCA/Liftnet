b305042a6171094b482f801348b06abe462522c1
Jorge Cristancho <damjorgecristancho@gmail.com>
2026-04-21 17:18:16 +0200

    hanin

---
+++ liftnet-frontend/src/app/documentosHanin/postulante/postulante.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-postulante',
+  imports: [],
+  templateUrl: './postulante.html',
+  styleUrl: './postulante.css',
+})
+export class Postulante {}
